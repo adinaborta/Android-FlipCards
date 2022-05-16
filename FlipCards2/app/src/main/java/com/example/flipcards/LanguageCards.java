55c51957d5797037be5bd1b3828bc3e5ee8768d6
andy <andreeavoicu2001@gmail.com>
2022-05-16 18:19:26 +0300

    Local notification with deeplink

--- FlipCards2/app/src/main/java/com/example/flipcards/LanguageCards.java
+++ FlipCards2/app/src/main/java/com/example/flipcards/LanguageCards.java
@@ -1,13 +1,24 @@
 package com.example.flipcards;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Icon;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -30,10 +41,12 @@ public class LanguageCards extends AppCompatActivity implements OnCardDeckClickL
     private AdaptorLanguageCards adapter;
     private RecyclerView.LayoutManager layoutManager;
     private ArrayList<CardDeckItem> cardDecks;
+    private NotificationManager notificationManager;
 
     public static String CARD_ENGLISH = "";
     public static String CARD_FOREIGN = "";
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,7 +60,6 @@ public class LanguageCards extends AppCompatActivity implements OnCardDeckClickL
         cardDecks.add(new CardDeckItem("Instruments", "", R.drawable.guitar));
         cardDecks.add(new CardDeckItem("Vehicles", "", R.drawable.car));
         cardDecks.add(new CardDeckItem("Sweets", "", R.drawable.icecream));
-
 
         reciclerView = findViewById(R.id.cardDecks);
         reciclerView.setHasFixedSize(true);
@@ -86,6 +98,78 @@ public class LanguageCards extends AppCompatActivity implements OnCardDeckClickL
                 //testingShowAlertDialogButtonClicked(v, searchText.getText().toString());
             }
         });
+
+        createNotificationChannel();
+        // local notif function
+        Button button2 = (Button) findViewById(R.id.notifbutton);
+        button2.setOnClickListener(new View.OnClickListener() {
+            private static final String CHANNEL_ID = "cardNotifyChannel";
+
+            public void onClick(View v) {
+                EditText notifText = findViewById(R.id.notiftext);
+                sendNotification(v, notifText.getText().toString());
+            }
+        });
+
+        // local notification ?
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String filterStr = intent.getStringExtra("Filter");
+        // started with deeplink?
+        if (filterStr != null) {
+            searchText.setText(filterStr);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void createNotificationChannel() {
+        notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        int importance = NotificationManager.IMPORTANCE_LOW;
+        NotificationChannel channel = new NotificationChannel("fmi.gr3xy.cards", "cards", importance);
+
+        channel.setDescription("my description");
+        channel.enableLights(true);
+        channel.setLightColor(Color.RED);
+        channel.enableVibration(true);
+        channel.setVibrationPattern(
+                new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+        notificationManager.createNotificationChannel(channel);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    protected void sendNotification(View view, String card) {
+
+        int notificationID = 101;
+        Intent resultIntent = new Intent(this, LanguageCards.class);
+        resultIntent.putExtra("Filter", card);
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(
+                        this,
+                        0,
+                        resultIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        final Icon icon = Icon.createWithResource(LanguageCards.this,
+                android.R.drawable.ic_dialog_info);
+
+        Notification.Action action =
+                new Notification.Action.Builder(icon, "Open", pendingIntent)
+                        .build();
+
+        String channelID = "fmi.gr3xy.cards";
+
+        Notification notification =
+                new Notification.Builder(LanguageCards.this,
+                        channelID)
+                        .setContentTitle("New Message")
+                        .setContentText("Open card: " + card)
+                        .setSmallIcon(android.R.drawable.ic_dialog_info)
+                        .setChannelId(channelID)
+                        .setContentIntent(pendingIntent)
+                        .setActions(action)
+                        .build();
+
+        notificationManager.notify(notificationID, notification);
     }
 
     private void shareText(String message) {
@@ -144,7 +228,7 @@ public class LanguageCards extends AppCompatActivity implements OnCardDeckClickL
 
     }
 
-
+// used for testing
     private void testingShowAlertDialogButtonClicked(View view, String message) {
 
         // setup the alert builder
